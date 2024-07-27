537658fb07e21fa364f99a792582c04b812e9cc8
Brendon-Chen <brendonchenwork@gmail.com>
2024-07-26 19:41:03 -0700

    add data, and cleaning script

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# SQL_Layoffs_Project
+# SQL Layoffs Project
+
+The purpose of this project is to learn SQL syntax and techniques and also data cleaning + exploration. The project features data cleaning and exploratory data analysis on a real data set about layoffs. There are *1995* entries and *9* columns (*company, location, industry, total\_laid\_off, percentage\_laid\_off, date, stage, country, funds\_raised\_millions*).
+
+## Cleaning
+Looking at the data, I removed duplicate entries using *row\_number()* to enumerate unique entries and then deleted entries with count > 1. After that I standardized the data: trim extra spacing/unnecessary characters (such as '.' from 'United States.'), combined industries that were meant to be one (such as Crypto and Crypto Currency), altered the date column to be of *date* type instead of *text*. I also filled in any null/blank values that I could and removed the rows that did not have layoff information.
+
+#### You can contact me at brendonchenwork@gmail.com
+#### Or visit my website to find me on other platforms such as Linkedin: [Click Here](https://brendon-chen.github.io/BrendonChen/)
---
+++ layoffs_script_clean.sql
@@ -0,0 +1,112 @@
+-- Create staging table
+create table layoffs_staging
+like layoffs;
+
+-- Insert data into staging
+insert layoffs_staging
+select * from layoffs;
+
+-- Display staging
+select * from layoffs_staging;
+
+-- lists all entries that are duplicates
+with duplicates_cte as
+(
+select *,
+row_number() over(partition by company, location, industry, total_laid_off, percentage_laid_off, `date`, stage, country, funds_raised_millions) row_num
+from layoffs_staging
+)
+select *
+from duplicates_cte
+where row_num > 1;
+
+create table layoffs_staging2
+like layoffs_staging;
+
+ALTER TABLE layoffs_staging2
+ADD row_num INT;
+
+select * from layoffs_staging2;
+
+INSERT INTO layoffs_staging2
+select *,
+row_number() over(partition by company, location, industry, total_laid_off, percentage_laid_off, `date`, stage, country, funds_raised_millions) row_num
+from layoffs_staging;
+
+SELECT * FROM layoffs_staging2
+WHERE row_num > 1;
+
+-- Delete duplicates in staging2
+DELETE FROM layoffs_staging2
+WHERE row_num > 1;
+
+
+-- Standardizing the data
+SELECT company, trim(company)
+FROM layoffs_staging2;
+
+UPDATE layoffs_staging2
+SET company = TRIM(company);
+
+SELECT DISTINCT industry FROM layoffs_staging2
+WHERE industry like '%Crypto%';
+
+UPDATE layoffs_staging2
+SET industry = 'Crypto'
+WHERE industry LIKE 'Crypto%';
+
+SELECT DISTINCT country FROM layoffs_staging2;
+
+UPDATE layoffs_staging2
+SET country = TRIM(TRAILING '.' FROM country)
+WHERE country LIKE 'United States%';
+
+SELECT `date`, str_to_date(`date`, '%m/%d/%Y')
+FROM layoffs_staging2;
+
+UPDATE layoffs_staging2
+SET `date` = str_to_date(`date`, '%m/%d/%Y');
+
+SELECT `date`
+FROM layoffs_staging2;
+
+ALTER TABLE layoffs_staging2
+MODIFY COLUMN `date` DATE;
+
+-- looking at nulls
+SELECT *
+FROM layoffs_staging2
+WHERE industry IS NULL
+OR industry = '';
+
+SELECT * FROM layoffs_staging2 WHERE company = 'Airbnb';
+
+SELECT t1.industry, t2.industry
+FROM layoffs_staging2 t1
+JOIN layoffs_staging2 t2
+ON t1.company = t2.company
+AND t1.location = t2.location
+WHERE (t1.industry IS NULL OR t1.industry = '')
+AND t2.industry IS NOT NULL;
+
+UPDATE layoffs_staging2
+SET industry = null
+WHERE industry = '';
+
+UPDATE layoffs_staging2 t1
+JOIN layoffs_staging2 t2
+ON t1.company = t2.company
+SET t1.industry = t2.industry
+WHERE (t1.industry IS NULL)
+AND t2.industry IS NOT NULL;
+
+SELECT * FROM layoffs_staging2
+WHERE total_laid_off IS NULL
+AND percentage_laid_off IS NULL;
+
+DELETE FROM layoffs_staging2
+WHERE total_laid_off IS NULL
+AND percentage_laid_off IS NULL;
+
+ALTER TABLE layoffs_staging2
+DROP COLUMN row_num;
